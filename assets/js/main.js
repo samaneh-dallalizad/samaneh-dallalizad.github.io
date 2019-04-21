c2d38a9ae289616343fa3af8308f84e223d7622f
Samaneh Dallalizad <samaneh.dallalizad@gmail.com>
2019-04-21 17:51:50 +0200

    Initialize repo

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function goTop(){
+        event.preventDefault();           
+        window.scrollTo({
+            top: 0,            
+            behavior:"smooth"
+            })           
+
+     }
